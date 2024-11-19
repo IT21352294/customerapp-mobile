28712c33487aa941db44923d233d1ba77e9902a0
jrkavinda <it21352294@my.sliit.lk>
2024-11-19 12:01:29 +0530

    create home page

---
+++ app/policy.tsx
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native"
+
+export default function PolicyScreen(){
+    return(
+        <View>
+            <Text style={{fontSize: 30}}>Policy Screen</Text>
+        </View>
+
+    )
+}
